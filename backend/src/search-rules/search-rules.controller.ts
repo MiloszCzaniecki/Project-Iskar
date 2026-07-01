4ff7a4ee53926731accf9c70b3ace601df13981d
Miłosz Czaniecki <115213738+MiloszCzaniecki@users.noreply.github.com>
2026-07-01 20:12:57 +0000

    Functional controller and service for search rules module

--- backend/src/search-rules/search-rules.controller.ts
+++ backend/src/search-rules/search-rules.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { SearchRulesService } from './search-rules.service';
 
 @Controller('search-rules')
-export class SearchRulesController {}
+export class SearchRulesController {
+  constructor(private readonly searchRulesService: SearchRulesService) {}
+  // Above is dependency injection
+
+  @Get()
+  findAll() {
+    return this.searchRulesService.findAll();
+  }
+}
--- backend/src/search-rules/search-rules.service.ts
+++ backend/src/search-rules/search-rules.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SearchRulesService {}
+export class SearchRulesService {
+    private readonly searchRules = [
+        {
+            id: '1',
+            keyword: 'Angular',
+            isActive: true,
+        },
+        {
+            id: '2',
+            keyword: 'NestJS',
+            isActive: true,
+        },
+        {
+            id: '3',
+            keyword: 'TypeScript',
+            isActive: true,
+        },
+    ];
+
+    findAll() {
+        return this.searchRules;
+    }
+}
